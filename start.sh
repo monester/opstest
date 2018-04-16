a0a1cd2e91965157f311da3d3e7f30582c03b20e
Aleksandr Charykov <monester@gmail.com>
2018-04-16 10:41:47 +0200

    Add set -x

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 /bin/systemctl daemon-reload
 /bin/systemctl enable test_app
 /bin/systemctl start test_app
--- stop.sh
+++ stop.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 if test -f /etc/systemd/system/test_app.service; then
     /bin/systemctl stop test_app
 fi
